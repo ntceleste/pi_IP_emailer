06b5e495cfdb8f3b6aa4f5c76bfda9c80e3f7806
Nate <nate@Nates-MacBook-Pro.local>
2019-01-17 14:42:27 -0500

    fixed ip grabbing for pi

--- IPEmailer.py
+++ IPEmailer.py
@@ -1,5 +1,17 @@
 import smtplib
 import socket
+import fcntl
+import struct
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 print(server.starttls())
@@ -7,6 +19,6 @@ server.login("natelampimailer@gmail.com", "lampimailer377")
 
 hostname = socket.gethostname()
 ip_addr = socket.gethostbyname(hostname)
-msg = ip_addr
+msg = get_ip_address('wlan0')
 server.sendmail("natelampimailer@gmail.com", "ntc14@case.edu", msg)
 server.quit()
